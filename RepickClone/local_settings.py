ab694e9a420fcb62c9264bfbe969a8852c4a609f
SergioAnd95 <developinc@yadnex.ru>
2017-08-11 15:59:53 +0300

    update confing for heroku

--- RepickClone/local_settings.py
+++ RepickClone/local_settings.py
@@ -1,1 +1,13 @@
+import os
+
+from .settings import BASE_DIR
+
+
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- RepickClone/settings.py
+++ RepickClone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9u2+2sl1_a(wdi4&yquq)6vlffl@3kxzk*6st_g@r5rq0x*0t-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -83,11 +84,12 @@ WSGI_APPLICATION = 'RepickClone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
