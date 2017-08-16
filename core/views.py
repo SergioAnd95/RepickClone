1a47e0125e89e1bd6bf816d5fc9e0c15044c8a2f
SergioAnd95 <developinc@yadnex.ru>
2017-08-16 17:47:58 +0300

    add filtration to main page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView
 
 from catalogue.models import Item
+from catalogue.forms import MainPageItemFilter
 
 # Create your views here.
 
@@ -10,3 +11,23 @@ class MainPageListView(ListView):
     template_name = 'index.html'
     context_object_name = 'items'
     paginate_by = 20
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if 'order_by' in self.request.GET:
+            self.filter_form = MainPageItemFilter(self.request.GET, items_qs=qs)
+        else:
+            self.filter_form = MainPageItemFilter(items_qs=qs)
+
+        if self.filter_form.is_valid():
+            qs = self.filter_form.filter_data()
+
+        if self.request.is_ajax():
+            self.template_name='catalogue/item_list.html'
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['filter_form'] = self.filter_form
+        return ctx
