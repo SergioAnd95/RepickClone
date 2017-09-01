1f17ca2c814ce0e09784493f1a5213cec4f8d92c
SergioAnd95 <developinc@yadnex.ru>
2017-09-01 17:26:20 +0300

    add bonsai elastic

--- core/views.py
+++ core/views.py
@@ -9,11 +9,11 @@ from catalogue.forms import MainPageItemFilter
 class MainPageListView(ListView):
     model = Item
     template_name = 'index.html'
-    context_object_name = 'items'
-    paginate_by = 20
+    context_object_name = 'items_list'
 
     def get_queryset(self):
         qs = super().get_queryset()
+
         if 'order_by' in self.request.GET:
             self.filter_form = MainPageItemFilter(self.request.GET, items_qs=qs)
         else:
@@ -23,9 +23,9 @@ class MainPageListView(ListView):
             qs = self.filter_form.filter_data()
 
         if self.request.is_ajax():
-            self.template_name='catalogue/item_list.html'
+            self.template_name='catalogue/item_list_page.html'
 
-        return qs
+        return qs[:18]
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
