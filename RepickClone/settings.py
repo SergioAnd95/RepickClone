f9aecd5a4e373b9d8e8b45d0f6700a2272bb8b02
SergioAnd95 <developinc@yadnex.ru>
2017-09-01 16:22:09 +0300

    add bonsai elasticsearch

--- RepickClone/settings.py
+++ RepickClone/settings.py
@@ -154,7 +154,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack_elasticsearch.elasticsearch5.Elasticsearch5SearchEngine',
-        'URL': 'localhost:9200',
+        'URL': os.environ.get('BONSAI_URL'),
         'INDEX_NAME': 'haystack',
         'INCLUDE_SPELLING': True,
     },
